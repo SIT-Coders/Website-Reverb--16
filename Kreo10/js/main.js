bc386e0159629c99cb3aeec300d0856d8ebceda7
Karan Sharma <karan1276@gmail.com>
2016-01-25 07:57:07 +0530

    updates

--- Kreo10/js/main.js
+++ Kreo10/js/main.js
@@ -70,6 +70,18 @@
 	      randomize: false,
 	   });
 
+//	   $('#sponsors-slider').flexslider({
+//	   	namespace: "flex-",
+//	      controlsContainer: "",
+//	      animation: 'slide',
+//	      controlNav: true,
+//	      directionNav: false,
+//	      smoothHeight: true,
+//	      slideshowSpeed: 7000,
+//	      animationSpeed: 600,
+//	      randomize: false,
+//	   });
+	   
    });
 
 
